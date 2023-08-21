19f380990737586541e0d41e4b1cfb7bc7dd69c6
“bcmm.intern08” <bcmm.intern08@brycenmyanmar.com.mm>
2023-08-21 09:51:41 +0630

    deploy with github pages

--- src/compoents/UpdateProduct.js
+++ src/compoents/UpdateProduct.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import Header from "./Header";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import { useParams } from "react-router-dom";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
 
 const UpdateProduct = () => {
   const [product, setProduct] = useState([]);
   const { id } = useParams();
   const navigate = useNavigate();
+
+  // const [name, setName] = useState();
+  // const [file, setFile] = useState();
+  // const [price, setPrice] = useState();
+  // const [description, setDescription] = useState();
 
   const nameRef = useRef(null);
   const fileRef = useRef(null);
@@ -28,6 +31,8 @@ const UpdateProduct = () => {
   const path = product.file_path + "";
   const modifiedFilePath = path.replace("public/", "");
 
+  
+
   const updateProduct = async (e) => {
     e.preventDefault();
 
@@ -38,28 +43,32 @@ const UpdateProduct = () => {
       description: descriptionRef.current.value,
     };
 
-    const formData = new FormData();
-    formData.append("file", updatedProduct.file);
-    formData.append("price", updatedProduct.price);
-    formData.append("description", updatedProduct.description);
-    formData.append("name", updatedProduct.name);
+    
+    const formData = new FormData();    
+    formData.append('file', updatedProduct.file);
+    formData.append('price', updatedProduct.price);
+    formData.append('description', updatedProduct.description);
+    formData.append('name', updatedProduct.name);
 
     try {
-      const result = await fetch(`http://127.0.0.1:8000/api/updateProduct/${id}`, {
-        method: "POST",
-        body: formData,
-      });
+        const result = await fetch(`http://127.0.0.1:8000/api/updateProduct/${id}`, {
+            method: 'POST',
+            body: formData
+        });
 
-      if (result.ok) {
-        navigate("/");
-      } else {
-        alert("Failed to update data");
-      }
+        if (result.ok) {
+          navigate('/');
+
+        } else {
+            alert("Failed to update data");
+        }
     } catch (error) {
-      console.error("Error updating data:", error);
+        console.error("Error updating data:", error);
     }
-    alert("Data has been updated successfully");
-  };
+    alert("Data has been updated successfully")
+};
+
+
 
   return (
     <div>
@@ -67,53 +76,49 @@ const UpdateProduct = () => {
       <h1>UpdateProduct</h1>
       {product && (
         <form className="col-sm-6 offset-sm-3" onSubmit={updateProduct}>
-          <TextField 
-            label="Name"
-            value={product.name}
-            inputRef={nameRef}
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            required
+        <input
+          type="text"
+          className="form-control mt-2"
+          defaultValue={product.name}
+          ref={nameRef}
+          placeholder="Name"
+          required
+        />
+        <input
+          type="file"
+          className="form-control mt-2"
+          ref={fileRef}
+          placeholder="File"
+          required
+        />
+        <input
+          type="text"
+          className="form-control mt-2"
+          defaultValue={product.price}
+          ref={priceRef}
+          placeholder="Price"
+          required
+        />
+        <input
+          type="text"
+          className="form-control mt-2"
+          defaultValue={product.description}
+          ref={descriptionRef}
+          placeholder="Description"
+          required
+        />
+        <div className="mt-2">
+          <img
+            style={{ width: 100 }}
+            src={`http://127.0.0.1:8000/storage/${modifiedFilePath}`}
+            alt="Product"
           />
-       
-          <input
-            type="file"
-            className="form-control mt-2"
-            ref={fileRef}
-            placeholder="File"
-            required
-          />
-          <TextField id="outlined-helperText"
-            label="Price"
-            value={product.price}
-            inputRef={priceRef}
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            required
-          />
-          <TextField id="outlined-helperText"
-            label="Description"
-            value={product.description}
-            inputRef={descriptionRef}
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            required
-          />
-          <div className="mt-2">
-            <img
-              style={{ width: 100 }}
-              src={`http://127.0.0.1:8000/storage/${modifiedFilePath}`}
-              alt="Product"
-            />
-          </div>
-
-          <Button type="submit" variant="contained" color="primary" className="mt-3">
-            Update Product
-          </Button>
-        </form>
+        </div>
+  
+        <button type="submit" className="btn btn-primary mt-3">
+          Update Product
+        </button>
+      </form>
       )}
     </div>
   );
